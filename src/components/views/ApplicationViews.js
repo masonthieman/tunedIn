f29c5bbacbe06ddaf441614e32ff6d99b18d2b3e
Mason Thieman <masonthieman@gmail.com>
2022-08-11 15:13:23 -0500

    implemented reactstrap with navbar

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,5 +1,5 @@
 import { Outlet, Route , Routes } from "react-router-dom"
-import { NavBar } from "../nav/NavBar.js"
+import { UserNavBar } from "../nav/NavBar.js"
 import { EventList } from "../events/EventList.js"
 import { EventForm } from "../events/EventForm.js"
 
@@ -10,7 +10,7 @@ export const ApplicationViews = () => {
             <Route path="/" element={
                 <>
                     <h1>TunedIn</h1>
-                    <NavBar />
+                    <UserNavBar />
                     <Outlet />
                 </>
             }>
